c8a721baaddbd0d5eb919ad754e48abce6e75360
大蘑菇 <441079498@qq.com>
2019-01-16 18:56:47 +0800

    Update predata.py

--- predata.py
+++ predata.py
@@ -6,3 +6,12 @@ pca.fit(data)
 print(pca.components_)
 #贡献率
 print(pca.explained_variance_ratio_)
+
+#建立决策树模型，基于信息熵
+from sklearn.tree import DecisionTreeClassifier as DTC
+dtc=DTC(criterion='entropy')
+#训练模型
+dtc.fit(x,y)
+
+from sklearn.tree import export_graphviz
+from sklearn.externals.six import StringIO
